9a2ebbce82d6dcb302d21ad36b20f97698dcfe02
Edwin Lara <edu.lara.lev@gmail.com>
2018-08-16 16:35:04 -0500

    avance

--- app/src/main/java/com/example/itlab/authenticationfirebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/itlab/authenticationfirebaseapp/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.itlab.authenticationfirebaseapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<View>(R.id.loginBtn) as Button
+        val regTxt = findViewById<View>(R.id.regTxt) as TextView
+
+        loginBtn.setOnClickListener({
+            view -> login()
+        })
+
+        regTxt.setOnClickListener({
+            view ->  register()
+        })
+
+
+    }
+
+    private fun register() {
+        startActivity(Intent(this, Register::class.java))
+    }
+
+    private fun login() {
+        val emailTxt = findViewById<View>(R.id.emailTxt) as EditText
+        val passwordTxt = findViewById<View>(R.id.passwordTxt) as EditText
+
+        var email = emailTxt.text.toString()
+        var password = passwordTxt.text.toString()
+
+        if(!email.isEmpty() && !password.isEmpty()){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, {
+                task ->
+                run {
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, Timeline::class.java))
+                    }else{
+                        Toast.makeText(this,"Error", Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+        }else{
+            Toast.makeText(this, "Ingresar credenciales", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
