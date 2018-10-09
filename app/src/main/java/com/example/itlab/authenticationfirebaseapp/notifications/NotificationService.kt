7c87595422682241972af20d56d856c7af77dec9
Ismael Limo <ismael.limo@gmail.com>
2018-10-09 18:08:46 -0500

    últimos cambios.

--- app/src/main/java/com/example/itlab/authenticationfirebaseapp/notifications/NotificationService.kt
+++ app/src/main/java/com/example/itlab/authenticationfirebaseapp/notifications/NotificationService.kt
@@ -1,56 +1,44 @@
 package com.example.itlab.authenticationfirebaseapp.notifications
 
-import android.os.Build
-import android.support.annotation.RequiresApi
+import android.app.NotificationManager
+import android.content.Context
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.example.itlab.authenticationfirebaseapp.BuildConfig
+import com.example.itlab.authenticationfirebaseapp.R
+import com.example.itlab.authenticationfirebaseapp.shared.Constants
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.util.*
 
 class NotificationService : FirebaseMessagingService() {
-    //Declaramos la variable que instancia la clase NotificationManager.
 
-    //Implementando evento onNewToken para obtener nuevo token.
+    private lateinit var notificationManager: NotificationManager
+
     override fun onNewToken(token: String) {
-        //LLamando a método súper de onNewToken.
-
-        //Obtener nuevo token.
+        super.onNewToken(token)
+        Log.e(Constants.TAG, "Refreshed token: $token")
     }
 
-    //Implementando evento onMessageReceived para recibir mensajes.
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        //Obteniendo el servicio de notificaciones del sistema Android como administrador de notificaciones.
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        //LLamando a método de compatiblidad con Android Oreo.
+        val notificationId = Random().nextInt(60000) + (System.currentTimeMillis() and 0xfffffff).toInt()
 
-        //Generando ID para cada notificación que se va a agregar al centro de notificaciones de Android.
+        val title = remoteMessage.notification?.title
+        val message = remoteMessage.notification?.body
 
-        //Recibiendo título del mensaje.
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        //Recibiendo cuerpo del mensaje.
+        val notificationBuilder = NotificationCompat.Builder(this, BuildConfig.DEMO_CHANNEL_ADMIN_ID)
+                .setSmallIcon(R.drawable.ic_notifications_active)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
 
-        //Obtener sonido por defecto para reproducirlo cuando llegue una nueva notificación.
-
-        //Construyendo nueva notificación.
-
-        //Agregando notificación al centro de notificaciones.
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private fun setupChannels() {
-        //Obteniendo nombre de canal de notificaciones del archivo strings.xml.
-
-        //Obteniendo descripción de canal de notificaciones del archivo strings.xml.
-
-        //Instanciando canal de notificaciones
-
-        //Generando nuevo canal de notificaciones con un ID previamente almacenado en la configuración de la aplicación y el nombre obtenido del archivo strings.xml.
-
-        //Añadiendo descripción al canal de notificaciones.
-
-        //Habilitando y configurando LED de notificaciones.
-
-        //Habilitando vibraciones.
-
-        //Creando canal de notificaciones.
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
 }
