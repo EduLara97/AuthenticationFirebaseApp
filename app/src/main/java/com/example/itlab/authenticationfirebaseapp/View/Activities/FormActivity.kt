1b83a7198d9b02c2b9a4319f9dcd5d79346a4924
Anton Tchistiakova <atch97@gmail.com>
2018-10-09 14:37:33 -0500

    fix

--- app/src/main/java/com/example/itlab/authenticationfirebaseapp/View/Activities/FormActivity.kt
+++ app/src/main/java/com/example/itlab/authenticationfirebaseapp/View/Activities/FormActivity.kt
@@ -6,8 +6,10 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.design.widget.TextInputLayout
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
@@ -71,8 +73,14 @@ class FormActivity : AppCompatActivity(), FormDelegate {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item!!.itemId) {
             R.id.check -> {
-                val bitmapDrawable = imageView.drawable as BitmapDrawable
-                mPresenter.uploadImage(bitmapDrawable.bitmap)
+                if (checkForCompletion()) {
+                    if (imageView.drawable !is ColorDrawable) {
+                        val bitmapDrawable = imageView.drawable as BitmapDrawable
+                        mPresenter.uploadImage(bitmapDrawable.bitmap)
+                    } else {
+                        onImageUploaded("")
+                    }
+                }
             }
             android.R.id.home -> {
                 onBackPressed()
@@ -89,5 +97,27 @@ class FormActivity : AppCompatActivity(), FormDelegate {
     override fun onImageUploaded(imageUID: String) {
         val values = mPresenter.toHashMap(txtNameInput.text.toString(), txtEmailInput.text.toString(), txtTelephoneInput.text.toString(), imageUID)
         mPresenter.saveContact(values)
+    }
+
+    private fun checkForCompletion(): Boolean {
+        var complete = true
+        if (txtNameInput.text.toString() == "") {
+            setError(nameInputLayout)
+            complete = false
+        }
+        if (txtEmailInput.text.toString() == "") {
+            setError(emailInputLayout)
+            complete = false
+        }
+        if (txtTelephoneInput.text.toString() == "") {
+            setError(telephoneInputLayout)
+            complete = false
+        }
+
+        return complete
+    }
+
+    private fun setError(layout: TextInputLayout) {
+        layout.error = getString(R.string.obligatoryError)
     }
 }
