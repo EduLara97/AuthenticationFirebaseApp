0e1b1decf4f8523dec506d20af54705f180ac2be
Ismael Limo <ismael.limo@gmail.com>
2018-10-09 18:22:10 -0500

    Fix del merge.

--- app/src/main/java/com/example/itlab/authenticationfirebaseapp/notifications/NotificationService.kt
+++ app/src/main/java/com/example/itlab/authenticationfirebaseapp/notifications/NotificationService.kt
@@ -1,22 +1,17 @@
 package com.example.itlab.authenticationfirebaseapp.notifications
 
-import android.os.Build
-import android.support.annotation.RequiresApi
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
-
-class NotificationService : FirebaseMessagingService() {
+class NotificationService () {
     //Declaramos la variable que instancia la clase NotificationManager.
 
     //Implementando evento onNewToken para obtener nuevo token.
-    override fun onNewToken(token: String) {
+    fun onNewToken(token: String) {
         //LLamando a método súper de onNewToken.
 
         //Obtener nuevo token.
     }
 
     //Implementando evento onMessageReceived para recibir mensajes.
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    fun onMessageReceived(remoteMessage: Any) {
         //Obteniendo el servicio de notificaciones del sistema Android como administrador de notificaciones.
 
         //Generando ID para cada notificación que se va a agregar al centro de notificaciones de Android.
