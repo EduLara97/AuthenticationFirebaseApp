426b9c4bff48e08202c559bff7e8f489953b26b1
Carlos Ugaz <30916886+carlosgub@users.noreply.github.com>
2018-10-17 11:45:37 -0500

    FIX ACTIVITY

--- app/src/main/java/com/example/itlab/authenticationfirebaseapp/views/activities/MLActivity.kt
+++ app/src/main/java/com/example/itlab/authenticationfirebaseapp/views/activities/MLActivity.kt
@@ -31,7 +31,7 @@ class MLActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ml)
 
 
         /**Verificar si se tiene el permiso de la camara*/
